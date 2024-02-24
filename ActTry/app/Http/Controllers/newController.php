1a7c37ffa68d1f820e336ba1f23e27e70cac01a3
Rhealoverrrre <balabarhealove323@gmail.com>
2024-02-24 02:33:30 -0800

    First Push to Repository

---
+++ ActTry/app/Http/Controllers/newController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Http\Response;
+
+class UserController extends Controller
+{
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Return the list of users
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return response()->json(['data' => $users], 200);
+    }
+    
+    /**
+     * Add a new user
+     * @return \Illuminate\Http\Response
+     */
+    public function add(Request $request)
+    {
+        $rules = [
+            'username' => 'required|max:20',
+            'password' => 'required|max:20',
+            'gender' => 'required|in:Male,Female',
+        ];
+
+        $this->validate($request, $rules);
+        $user = User::create($request->all());
+        return $this->successResponse($user, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Obtains and show one user
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return $this->successResponse($user);
+    }
+
+    /**
+     * Update an existing user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $rules = [
+            'username' => 'max:20',
+            'password' => 'max:20',
+            'gender' => 'in:Male,Female',
+        ];
+
+        $this->validate($request, $rules);
+        $user = User::findOrFail($id);
+
+        $user->fill($request->all());
+        // if no changes happen
+        if ($user->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $user->save();
+        return $this->successResponse($user);
+    }
+
+    /**
+     * Remove an existing user
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return $this->successResponse($user);
+    }
+
+    // Common response methods
+    protected function successResponse($data, $code = Response::HTTP_OK)
+    {
+        return response()->json($data, $code);
+    }
+
+    protected function errorResponse($message, $code)
+    {
+        return response()->json(['error' => $message, 'code' => $code], $code);
+    }
+}
